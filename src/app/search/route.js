735ebb6bd581040265c9a57255505be6b881080a
Daniel Kliewer <danielkliewer@gmail.com>
2025-03-06 09:42:49 -0600

    a

--- src/app/search/route.js
+++
@@ -1,8 +0,0 @@
-// This file ensures that the search page is treated as a dynamic route
-// and not pre-rendered during build time
-
-export const dynamic = 'force-dynamic';
-
-// This is a Next.js App Router route handler
-// It doesn't need to export any handlers since we're just using it
-// to configure the route behavior
